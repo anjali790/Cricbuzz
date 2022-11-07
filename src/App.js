34277edc4c6e757742cb2afdacb72abf6b3d18c6
anjali790 <anjaliruby790@gmail.com>
2022-11-07 23:55:54 +0530

    dashboard

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import 'antd/dist/antd.css';
+import Tables from './components/Tables';
+
+function App() {
+  return (
+    <>
+      <Tables />
+    </>
+  );
+}
+
+export default App;
