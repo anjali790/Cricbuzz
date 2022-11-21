032175e9ad502ebc598fd6a6404491c69d33c720
anjali790 <anjaliruby790@gmail.com>
2022-11-22 03:08:12 +0530

    dashboard

--- src/components/Table/CricbuzzTable.jsx
+++ src/components/Table/CricbuzzTable.jsx
@@ -8,7 +8,7 @@ import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 
-import './cricbuzz.css';
+import './cricbuzztable.css';
 
 export function CricbuzzTable() {
     const [tableData, setTableData] = useState([]);
