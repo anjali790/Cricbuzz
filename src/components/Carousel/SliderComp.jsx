3bc57c0ba4f30d9043533e1acc2a9aeb1c747248
anjali790 <anjaliruby790@gmail.com>
2022-11-22 19:52:59 +0530

    dashboard

--- src/components/Carousel/SliderComp.jsx
+++ src/components/Carousel/SliderComp.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
+import { Pagination, Navigation } from "swiper";
 import "swiper/css";
 import "swiper/css/pagination";
 import "swiper/css/navigation";
-import { Pagination, Navigation } from "swiper";
 
 import { Item } from '../Dashboard/ConstData';
 
